bd675f17b4ce512230a44a23263d015c27caeffc
Akkariin Meiko <akkariins@gmail.com>
2020-03-20 08:47:35 +0800

    Oops
    SQL wrong

--- esx_realparking.sql
+++ esx_realparking.sql
@@ -6,6 +6,6 @@ CREATE TABLE `car_parking`  (
   `time` bigint(20) NOT NULL,
   `parking` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 48 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 INSERT INTO `items`(`name`, `label`, `limit`) VALUES ('parkingcard', 'Parking Card', -1);
