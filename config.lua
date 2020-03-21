47a90818a1aa0a194c22bc5fcf0e587e3edac4f9
Akkariin Meiko <akkariins@gmail.com>
2020-03-21 13:31:33 +0800

    Update config.lua
    Add new config

--- config.lua
+++ config.lua
@@ -4,6 +4,8 @@
 
 Config = {}
 
+Config.UsingOldESX = true      -- If you are using ESX 1.2.0 or higher please leave this to false
+
 Config.ParkingLocations = {
 	parking1 = {
 		x      = -327.73,                               -- Central location X, Y, Z of the parking
@@ -33,3 +35,19 @@ Config.Locales = {
 	["owner"]            = "Owner: ~y~%s~s~",
 	["plate"]            = "Plate: ~g~%s~s~",
 }
+
+--[[ Chinese locales
+	["only_allow_car"]   = "你只能在這裡停放汽車",
+	["car_saved"]        = "你的車輛已經保存",
+	["press_to_save"]    = "按下 ~INPUT_CONTEXT~ 來保存你的車輛",
+	["not_your_car"]     = "你必須擁有這輛車的所有權才能保存",
+	["need_parking_fee"] = "按下 ~INPUT_CONTEXT~ 來繳納 ~g~￥%s元~s~ 停車費後即可離開",
+	["not_enough_money"] = "你沒有足夠的錢",
+	["parking_fee"]      = "停車費用：~g~￥%s元/天",
+	["pay_success"]      = "你已支付了停車費 ~g~￥%s元~s~，現在可以將車開走了",
+	["parking_full"]     = "這個停車場已經不能再停放更多的車了",
+	["invalid_car"]      = "你沒有在這個停車場存放這輛車",
+	["already_parking"]  = "這個停車場已經停著一輛相同車牌的車了",
+	["owner"]            = "車主：~y~%s~s~",
+	["plate"]            = "車牌：~g~%s~s~",
+]]--
