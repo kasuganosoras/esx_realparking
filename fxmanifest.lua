1727bf9e0ef588024ee1d4c8405c4d416a8521f9
Muhammad Hifzirrohman Syafi'i <hifzi@users.noreply.github.com>
2020-03-23 12:41:19 +0700

    Update and rename __resource.lua to fxmanifest.lua
    update to fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,8 @@
 --[[         FiveM Real Parking Script by Akkariin         ]]--
 --[[ ===================================================== ]]--
 
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+game 'gta5'
 
 client_scripts {
 	'@es_extended/locale.lua',
