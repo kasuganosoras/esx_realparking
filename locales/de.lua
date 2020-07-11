17412a3e76a58cbe8fd5cb91a1a0818568ecf198
Akkariin Meiko <akkariins@gmail.com>
2020-07-11 08:21:41 +0800

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,16 @@
+-- German translate by moistnutzz
+Locales['de'] = {
+	["only_allow_car"]   = "Du kannst nur hier Parken",
+	["car_saved"]        = "Dein Auto wurde eingeparkt",
+	["press_to_save"]    = "Drücke ~INPUT_CONTEXT~ um dein Auto einzuparken",
+	["not_your_car"]     = "Du musst dieses Auto besitzen um es einzuparken",
+	["need_parking_fee"] = "Drücke ~INPUT_CONTEXT~ um die Parkgebühren zu bezahlen ~g~$%s~s~",
+	["not_enough_money"] = "Du hast nicht genügend Geld",
+	["parking_fee"]      = "Parkgebühren: ~g~$%s/Tag",
+	["pay_success"]      = "Du hast die Parkgebühr von: ~g~$%s~s~ bezahlt, du kannst dein Auto nun fahren",
+	["parking_full"]     = "Dieser Parkplatz ist voll, du kannst hier nicht Parken",
+	["invalid_car"]      = "Du hast dieses Auto hier nicht eingeparkt oder hast keine Erlaubnis, dieses Auto zu fahren",
+	["already_parking"]  = "Dieses Auto ist schon auf dem Parkplatz mit dem selben Nummernschild",
+	["owner"]            = "Besitzer: ~y~%s~s~",
+	["plate"]            = "Nummernschild: ~g~%s~s~",
+}
